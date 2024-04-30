ddd2ce43f2f95f5a7d0ad82655219ae679c0bc25
jrozb91 <55283527+jrozb91@users.noreply.github.com>
2024-04-30 14:29:26 +0200

    wip

--- xyz-motor/xyz_main.py
+++ xyz-motor/xyz_main.py
@@ -15,15 +15,15 @@ usteps_set = 8
 
 usteps_mm = 40 * 256
 
-pb = PathBuilder(50, 50, 50, 150, 150, 150, 25, 25, 25)
+pb = PathBuilder(50, 50, 50, 150, 150, 150, 5, 5, 5)
 path = pb.generate_path()
-for i in path :
-    print(i)
+#for i in path :
+    #print(i)
 
 steps = len(path)
 
 print("Total steps = " + str(steps))
-print("Microsteps per mm = " + str(usteps_mm))
+print("Microsteps/mm = " + str(usteps_mm))
 
 x = [int(usteps_mm * t[0]) for t in path]
 y = [int(usteps_mm * t[1]) for t in path]
@@ -37,14 +37,45 @@ time.sleep(1)
 print("Zero position aftert reset home: " + str(xyz.get_actual_pos()))
 #xyz.goto_end()
 
+print("Moving to start position")
+xyz.goto_xyz_absolute(x[0], y[0], z[0])
+
 print("Starting scan")
-time.sleep(1)
 
 n = 0
+start = time.time()
 
 for i in path :
     xyz.goto_xyz_absolute(x[n], y[n], z[n])
     print("Position " + str(n+1) +"/" + str(steps) + ": " + str(xyz.get_actual_pos_mm(usteps_mm)))
+    
+    if n > 1:
+        elapsed = time.time() - start
+        elapsed_h, remainder = divmod(elapsed, 3600)
+        elapsed_m, elapsed_s = divmod(remainder, 60)
+
+        end = (steps-n) * (elapsed/n)
+        end_h, remainder = divmod(end, 3600)
+        end_m, end_s = divmod(remainder, 60)
+
+        elapsed_str = []
+        if elapsed_h > 0:
+            elapsed_str.append("{}h".format(int(elapsed_h)))
+            elapsed_str.append("{}m".format(int(elapsed_m)))
+        else:
+            elapsed_str.append("{}m".format(int(elapsed_m)))
+            elapsed_str.append("{}s".format(int(elapsed_s)))
+
+        end_str = []
+        if end_h > 0:
+            end_str.append("{}h".format(int(end_h)))
+            end_str.append("{}m".format(int(end_m)))
+        else:
+            end_str.append("{}m".format(int(end_m)))
+            end_str.append("{}s".format(int(end_s)))
+
+        print("Elapsed time: " + " ".join(elapsed_str) + ", estimated end in: " + " ".join(end_str))
+    
     n = n + 1
     time.sleep(0.1)
     #acquire data here
